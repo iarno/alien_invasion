bd7c97410f314e4f6f1449d13e26f44e1194c230
阿諾 <itliulili@gmail.com>
2022-07-14 21:56:07 +0800

    bullet

--- game_functions.py
+++ game_functions.py
@@ -5,15 +5,19 @@
 import sys
 import pygame
 from pygame.event import Event
+from bullet import Bullet
 from settings import Settings
 from ship import Ship
 
-def check_keydown_events(event:Event, ship:Ship):
+def check_keydown_events(event:Event, ai_settings, screen, ship, bullets:pygame.sprite.Group):
     """键盘按下"""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 def check_keyup_events(event, ship):
     """键盘松开"""
@@ -22,19 +26,23 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ship:Ship):
+def check_events(ai_settings, screen, ship, bullets):
     """监听按键和鼠标事件"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
-def update_screen(ai_settings:Settings, screen:pygame.Surface, ship:Ship):
+def update_screen(ai_settings:Settings, screen:pygame.Surface, ship:Ship, bullets:pygame.sprite.Group):
     """更新屏幕上的图像，并切换到新屏幕"""
     screen.fill(ai_settings.bg_color)
+
+    # 在飞船和外星人后面重绘所有子弹
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
 
     ship.blitme()
 
--- settings.py
+++ settings.py
@@ -12,3 +12,9 @@ class Settings():
         self.secren_height = 800
         self.bg_color = (230, 230, 230)
         self.ship_speed_factor = 1.5
+
+        """子弹设置"""
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
