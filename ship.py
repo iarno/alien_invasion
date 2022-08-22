736728809ae6b16791b5d20d8895864584a45395
阿諾 <itliulili@gmail.com>
2022-08-22 23:01:58 +0800

    外星人

--- ship.py
+++ ship.py
@@ -1,42 +1,45 @@
-"""
-添加飞船图像
-"""
 import pygame
+from pygame.sprite import Sprite
 
-from settings import Settings
+class Ship(Sprite):
 
-class Ship():
-    """飞船"""
-    def __init__(self, ai_settings:Settings, screen:pygame.Surface) -> None:
+    def __init__(self, ai_settings, screen):
+        """Initialize the ship, and set its starting position."""
+        super(Ship, self).__init__()
         self.screen = screen
         self.ai_settings = ai_settings
 
-        # 加载飞船图像并获取外接矩形
-        self.image = pygame.image.load("images/ship.bmp")
+        # Load the ship image, and get its rect.
+        self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
-        # 将每艘飞船放在屏幕底部中央
+        # Start each new ship at the bottom center of the screen.
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
-
-        # 在飞船的属性center中存储小数值
+        
+        # Store a decimal value for the ship's center.
         self.center = float(self.rect.centerx)
-
+        
+        # Movement flags.
         self.moving_right = False
         self.moving_left = False
-
-    def blitme(self):
-        """将图像绘制到屏幕"""
-        self.screen.blit(self.image, self.rect)
-
+        
+    def center_ship(self):
+        """Center the ship on the screen."""
+        self.center = self.screen_rect.centerx
+        
     def update(self):
-        """移动飞船✈️"""
+        """Update the ship's position, based on movement flags."""
+        # Update the ship's center value, not the rect.
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.ai_settings.ship_speed_factor
         if self.moving_left and self.rect.left > 0:
             self.center -= self.ai_settings.ship_speed_factor
-
-        self.rect.centerx = self.center
             
-        
+        # Update rect object from self.center.
+        self.rect.centerx = self.center
+
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
